710d60361bbcaf26cdc92f860f2e964083f7df48
Eric Dalquist <eric.dalquist@gmail.com>
2010-05-27 19:31:26 +0000

    NOJIRA a little test case working with the updatePage API, for use on a future version of confluence
    git-svn-id: https://source.jasig.org/sandbox/ConfluenceIrcLogger/trunk@20677 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ src/test/java/org/jasig/irclog/messages/ConfluenceClientTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-2009, Jasig, Inc.
+ * See license distributed with this file and available online at
+ * https://www.ja-sig.org/svn/jasig-parent/tags/rel-10/license-header.txt
+ */
+
+package org.jasig.irclog.messages;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.xmlrpc.XmlRpcClient;
+import org.apache.xmlrpc.XmlRpcException;
+
+/**
+ * @author Eric Dalquist
+ * @version $Revision$
+ */
+public class ConfluenceClientTest {
+    public void testPageUpdate() throws Exception {
+        final XmlRpcClient client = new XmlRpcClient("https://wiki.jasig.org/rpc/xmlrpc");
+        
+        final String token = this.call(client, "login", "irclogbot", "");
+        System.out.println(token);
+        
+        final Map<String, Object> page = this.call(client, "getPage", token, "SND", "uPortal TEST IRC Logs-2010-05-25");
+        System.out.println(page);
+        
+        //Add log data
+        final String newContent = page.get("content") + "\ntesting log data";
+        page.put("content", newContent);
+        
+        /*
+         * Page  updatePage(String token, Page  page, PageUpdateOptions  pageUpdateOptions)  
+         */
+        final Map<String, Object> pageUpdateOptions = new Hashtable<String, Object>();
+        pageUpdateOptions.put("minorEdit", true);
+        this.call(client, "updatePage", token, page, pageUpdateOptions);
+        
+        this.call(client, "logout", token);
+    }
+    
+    private <T> T call(XmlRpcClient client, String method, Object... args) throws XmlRpcException, IOException {
+        return (T)client.execute("confluence1." + method, new Vector<Object>(Arrays.asList(args)));
+    }
+}
